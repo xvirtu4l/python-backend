6e33c292e2ed62b9f7947d033b8f672296e7a66b
LÊ NGOC MINH <minhle1309@gmail.com>
2026-02-06 10:39:26 +0700

    Generate JWT

--- Day 10 Backend/factories/auth_factory.py
+++ Day 10 Backend/factories/auth_factory.py
@@ -1,8 +1,10 @@
 from factories.user_factory import get_user_usecase
 from usecases.auth_usecase import AuthUseCase
 from security.password_hasher_interface import PasswordHasherImpl
+from security.jwt_service import create_access_token
 
 def get_auth_usecase() -> AuthUseCase:
     user_usecase = get_user_usecase()
     password_hasher = PasswordHasherImpl()
-    return AuthUseCase(user_usecase, password_hasher)
+    create_token = create_access_token
+    return AuthUseCase(user_usecase, password_hasher, create_token)
